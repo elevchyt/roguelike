d2f511004caaa6fb46982e3679d04f66bd49ac1a
Ελευθέριος Χυτήρογλου <lehy64@gmail.com>
2021-08-26 14:03:57 +0300

    initialized 'Ensnare' & 'Poison Dart' scenes

---
+++ Scripts/Skills/Poison Dart.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+onready var Player = get_parent().get_parent()
+onready var PlayerSkills = Player.get_node('PlayerSkills')
+onready var TargetDestination = Player.get_node('Target').position
+onready var CameraNode = get_node("/root/World/Camera2D")
+
+# Run hit animation on target destination arrival
+func _process(delta):
+	if (position == TargetDestination):
+		# Camera Shake (only on successful hit)
+		if (PlayerSkills.hitSuccess == true):
+			CameraNode.shake(4, 0.01, 0.1)
+		
+		# Reset rotation & change animation
+		$AnimatedSprite.rotation = 0
+		$AnimatedSprite.animation = 'hit'
+		$AnimatedSprite.speed_scale = 2.5
+
+# Remove instance after 'hit' animation plays once
+func _on_AnimatedSprite_animation_finished():
+	if ($AnimatedSprite.animation == 'hit'):
+		queue_free()
