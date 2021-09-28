bea3f994eaa691fc148e3b547817347e0223ad4b
elevchyt <50188474+elevchyt@users.noreply.github.com>
2021-09-29 01:43:14 +0300

    (bugged) inventory items shift after item drop

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -486,14 +486,6 @@ func _process(delta):
 		HUD.get_node('ItemDetails').visible = false
 	# Drop Item
 	elif (itemsMode == true && Input.is_action_just_pressed("key_ctrl") && items.empty() == false):
-		# Leave inventory
-		HUD.get_node('Tween').stop_all()
-		HUD.get_node('TweenTextTooltip').interpolate_property(HUD.get_node('SkillsConfirmCancelButtons'), "position", HUD.get_node('SkillsConfirmCancelButtons').position, Vector2(0, 0), 0.4, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
-		HUD.get_node('TweenTextTooltip').start()
-		HUD.get_node('Inventory/Tween').interpolate_property(HUD.get_node('Inventory'), "position", HUD.get_node('Inventory').position, Vector2(-368, 688), 0.4, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
-		HUD.get_node('Inventory/Tween').start()
-		HUD.get_node('ItemDetails').visible = false
-		
 		# Check current tile for a collision with an item
 		RayTarget.set_cast_to(Vector2(0, 0))
 		RayTarget.force_raycast_update()
@@ -502,12 +494,22 @@ func _process(delta):
 		if (Ray.get_collider() == null):
 			itemsMode = false
 			
+			# Leave inventory
+			HUD.get_node('Tween').stop_all()
+			HUD.get_node('TweenTextTooltip').interpolate_property(HUD.get_node('SkillsConfirmCancelButtons'), "position", HUD.get_node('SkillsConfirmCancelButtons').position, Vector2(0, 0), 0.4, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
+			HUD.get_node('TweenTextTooltip').start()
+			HUD.get_node('Inventory/Tween').interpolate_property(HUD.get_node('Inventory'), "position", HUD.get_node('Inventory').position, Vector2(-368, 688), 0.4, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+			HUD.get_node('Inventory/Tween').start()
+			HUD.get_node('ItemDetails').visible = false
+			
+			# Create item node
 			var instance = objItem.instance()
 			instance.itemName = items[itemChooseIndex]
 			instance.set_texture(itemSlots[itemChooseIndex].texture)
 			instance.position = position
 			RootNode.add_child(instance)
 			
+			# Empty slot
 			items[itemChooseIndex] = null
 			itemsDamage[itemChooseIndex] = null
 			itemsDescription[itemChooseIndex] = null
@@ -516,6 +518,20 @@ func _process(delta):
 			itemSlots[itemChooseIndex].texture = null
 			itemSlots[itemChooseIndex].get_parent().scale = Vector2(1, 1)
 			
+			# Shift items in inventory
+			if (itemChooseIndex < 6):
+				for i in range(itemChooseIndex + 1, items.size()):
+					if (items[i] != null):
+						items[i - 1] = items[i]
+						itemsDamage[i - 1] = itemsDamage[i]
+						itemsDescription[i - 1] = itemsDescription[i]
+						itemsState[i - 1] = itemsState[i]
+						itemsType[i - 1] = itemsType[i]
+						
+						var index = GameManager.itemsNames.find(items[i])
+						itemSlots[i - 1].texture = load(GameManager.itemsSlotSprites[index])
+					else:
+						break
 			#end_turn()
 	# Cancel Target (leaves from targetMode AND skillMode)
 	elif (targetMode == true && Input.is_action_just_pressed("key_escape")):
